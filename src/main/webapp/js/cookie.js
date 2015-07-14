19c0527f36e1a7196e6a5bee969f2958119e880c
Yizhou Zhu <mkingzhu@gmail.com>
2015-07-14 15:52:21 +0800

    add cookie function to avoid backgroudId error

---
+++ src/main/webapp/js/cookie.js
@@ -0,0 +1,63 @@
+  function encode(s) {
+    return config.raw ? s : encodeURIComponent(s);
+  }
+  function decode(s) {
+    return config.raw ? s : decodeURIComponent(s);
+  }
+  function stringifyCookieValue(value) {
+    return encode(config.json ? JSON.stringify(value) : String(value));
+  }
+  function parseCookieValue(s) {
+    if (s.indexOf('"') === 0) {
+      s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+    }
+    try {
+      s = decodeURIComponent(s.replace(/\+/g, ' '));
+      return config.json ? JSON.parse(s) : s;
+    } catch (e) { }
+  }
+  function read(s, converter) {
+    var value = config.raw ? s : parseCookieValue(s);
+    return converter ? converter(value) : value;
+  }
+
+  var cookie = config = function (key, value, options) {
+    // Write
+    if (arguments.length > 1) {
+      if (typeof options.expires === 'number') {
+        var days = options.expires, t = options.expires = new Date();
+        t.setMilliseconds(t.getMilliseconds() + days * 864e+5);
+      }
+
+      return (document.cookie = [
+        encode(key), '=', stringifyCookieValue(value),
+        options.expires ? '; expires=' + options.expires.toUTCString() : '',
+        options.path ? '; path=' + options.path : '',
+        options.domain ? '; domain=' + options.domain : '',
+        options.secure ? '; secure' : ''
+      ].join(''));
+    }
+
+    // Read
+    var result = key ? undefined : {},
+      cookies = document.cookie ? document.cookie.split('; ') : [],
+      i = 0,
+      l = cookies.length;
+
+    for (; i < l; i++) {
+      var parts = cookies[i].split('='),
+        name = decode(parts.shift()),
+        cookie = parts.join('=');
+
+      if (key === name) {
+        result = read(cookie, value);
+        break;
+      }
+
+      if (!key && (cookie = read(cookie)) !== undefined) {
+        result[name] = cookie;
+      }
+    }
+
+    return result;
+  };
